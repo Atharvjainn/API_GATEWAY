3aed039374be1e8e6dc6ae307276b3dcfeadacc7
Atharvjainn <jainatharv20@gmail.com>
2025-11-18 00:19:22 +0530

    basic setup of the service..

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const app = express()
+const rateLimit = require('express-rate-limit');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { PORT } = require('./config/serverconfig')
 const morgan = require('morgan')
 
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000, // 2 minutes
+    max: 5 // limit each IP to 5 requests per windowMs
+});
 
 
 const startupserver = async() => {
 
     app.use(morgan('combined'))
-    app.use('bookingservice',createProxyMiddleware({target : "http:"
-        
-    }))
+    app.use(limiter)
+    app.use('/bookingservice',createProxyMiddleware({target : "http://localhost:3002/",changeOrigin : true}))
 
     app.listen(PORT,() => {
         console.log("server started");
     })
+
 }
 
 
