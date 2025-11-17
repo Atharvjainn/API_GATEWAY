7b8d2633209c86729771e6d5c5b9692fa2ed1403
Atharvjainn <jainatharv20@gmail.com>
2025-11-18 00:49:45 +0530

    added axios and http-proxy-middleware and rate limiter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const rateLimit = require('express-rate-limit');
+const axios = require('axios')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { PORT } = require('./config/serverconfig')
 const morgan = require('morgan')
@@ -15,6 +16,22 @@ const startupserver = async() => {
 
     app.use(morgan('combined'))
     app.use(limiter)
+
+    app.use('/bookingservice',async(req,res,next) => {
+        try {
+            const response = await axios.get('http://localhost:3001/api/v1/isAuthenticated',{
+            headers : {
+                "x-access-token" : req.headers['x-access-token']
+            }
+        })
+            console.log(response.data);
+            next()
+        } catch (error) {
+            return res.status(401).json({
+                message : "not authorised"
+            })
+        }
+    })
     app.use('/bookingservice',createProxyMiddleware({target : "http://localhost:3002/",changeOrigin : true}))
 
     app.listen(PORT,() => {
